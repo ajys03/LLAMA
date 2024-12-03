591f7de9573785517167cb57db2c42cad8bad5e5
Anny Wu <annyw@umich.edu>
2024-12-02 22:48:10 -0500

    ran library

--- library/srcs/redefinitions.c
+++ library/srcs/redefinitions.c
@@ -1,37 +1,37 @@
-/* Copyright (C) 2018, Derrick Greenspan and 		*
- * the University of Central Florida 			*
- * Licensed under the terms of the MIT License. 	*
- * Please see the LICENSE file for more information 	*/
+#include <stdlib.h>   // For perror and exit
+#include <stdio.h>
+#include <sys/mman.h> // For mmap
 
-#include "redefinitions.h"
 #ifdef __linux__
-#include <numa.h>
-#include <numaif.h>
+#include <numa.h>   // For NUMA functions and types like nodemask_t
+#include <numaif.h> // For mbind and other NUMA-related functions
 #endif
-#include <sys/mman.h>
 
-void *mlm_malloc(size_t size, int level)
-{
-	void *ptr;
-	nodemask_t nodemask;
+void *mlm_malloc(size_t size, int level) {
+  void *ptr;
 
-  ptr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+  ptr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE,
+             -1, 0);
   if (ptr == MAP_FAILED) {
     perror("mmap2");
     exit(1);
   }
 
-	nodemask_zero(&nodemask);
-	nodemask_set_compat(&nodemask, level);
+#ifdef __linux__
+  nodemask_t nodemask;
+  nodemask_zero(&nodemask);
+  nodemask_set_compat(&nodemask, level);
 
-  if (mbind(ptr, size, MPOL_PREFERRED, &nodemask, sizeof(unsigned long), MPOL_MF_MOVE) < 0) {
+  if (mbind(ptr, size, MPOL_PREFERRED, &nodemask, sizeof(unsigned long),
+            MPOL_MF_MOVE) < 0) {
     perror("mbind");
     exit(1);
   }
+#endif
 
-	return ptr;
+  return ptr;
 }
 
-void ariel_mlm_free(void *ptr)
-{
+void ariel_mlm_free(void *ptr) {
+  // Freeing logic here (if needed)
 }
